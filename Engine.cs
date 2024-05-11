33caabf5b6a137253335cdf6463e303d04e8e202
cosmin <cosmin.serban02@e-uvt.ro>
2024-05-11 08:40:26 +0300

    Added teleport and bomb-drop feature on right-click

--- Engine.cs
+++ Engine.cs
@@ -1,37 +1,40 @@
 using System.Text.Json;
+using Silk.NET.Input;
 using Silk.NET.Maths;
 using Silk.NET.SDL;
 using TheAdventure.Models;
 using TheAdventure.Models.Data;
-
+ 
 namespace TheAdventure
 {
     public class Engine
     {
         private readonly Dictionary<int, GameObject> _gameObjects = new();
         private readonly Dictionary<string, TileSet> _loadedTileSets = new();
-
+ 
         private Level? _currentLevel;
         private PlayerObject _player;
         private GameRenderer _renderer;
         private Input _input;
-
+ 
         private DateTimeOffset _lastUpdate = DateTimeOffset.Now;
         private DateTimeOffset _lastPlayerUpdate = DateTimeOffset.Now;
-
+ 
         public Engine(GameRenderer renderer, Input input)
         {
             _renderer = renderer;
             _input = input;
-
+ 
             _input.OnMouseClick += (_, coords) => AddBomb(coords.x, coords.y);
+ 
+            _input.OnRightMouseClick += (_, coords) => HiddenAbility(coords.x, coords.y);
         }
-
+ 
         public void InitializeWorld()
         {
             var jsonSerializerOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
             var levelContent = File.ReadAllText(Path.Combine("Assets", "terrain.tmj"));
-
+ 
             var level = JsonSerializer.Deserialize<Level>(levelContent, jsonSerializerOptions);
             if (level == null) return;
             foreach (var refTileSet in level.TileSets)
@@ -40,19 +43,19 @@ namespace TheAdventure
                 if (!_loadedTileSets.TryGetValue(refTileSet.Source, out var tileSet))
                 {
                     tileSet = JsonSerializer.Deserialize<TileSet>(tileSetContent, jsonSerializerOptions);
-
+ 
                     foreach (var tile in tileSet.Tiles)
                     {
                         var internalTextureId = _renderer.LoadTexture(Path.Combine("Assets", tile.Image), out _);
                         tile.InternalTextureId = internalTextureId;
                     }
-
+ 
                     _loadedTileSets[refTileSet.Source] = tileSet;
                 }
-
+ 
                 refTileSet.Set = tileSet;
             }
-
+ 
             _currentLevel = level;
             /*SpriteSheet spriteSheet = new(_renderer, Path.Combine("Assets", "player.png"), 10, 6, 48, 48, new FrameOffset() { OffsetX = 24, OffsetY = 42 });
             spriteSheet.Animations["IdleDown"] = new SpriteSheet.Animation()
@@ -70,20 +73,20 @@ namespace TheAdventure
             _renderer.SetWorldBounds(new Rectangle<int>(0, 0, _currentLevel.Width * _currentLevel.TileWidth,
                 _currentLevel.Height * _currentLevel.TileHeight));
         }
-
+ 
         public void ProcessFrame()
         {
             var currentTime = DateTimeOffset.Now;
             var secsSinceLastFrame = (currentTime - _lastUpdate).TotalSeconds;
             _lastUpdate = currentTime;
-
+ 
             bool up = _input.IsUpPressed();
             bool down = _input.IsDownPressed();
             bool left = _input.IsLeftPressed();
             bool right = _input.IsRightPressed();
             bool isAttacking = _input.IsKeyAPressed();
             bool addBomb = _input.IsKeyBPressed();
-
+ 
             if(isAttacking)
             {
                 var dir = up ? 1: 0;
@@ -106,12 +109,12 @@ namespace TheAdventure
             var itemsToRemove = new List<int>();
             itemsToRemove.AddRange(GetAllTemporaryGameObjects().Where(gameObject => gameObject.IsExpired)
                 .Select(gameObject => gameObject.Id).ToList());
-
+ 
             if (addBomb)
             {
                 AddBomb(_player.Position.X, _player.Position.Y, false);
             }
-
+ 
             foreach (var gameObjectId in itemsToRemove)
             {
                 var gameObject = _gameObjects[gameObjectId];
@@ -126,20 +129,20 @@ namespace TheAdventure
                 _gameObjects.Remove(gameObjectId);
             }
         }
-
+ 
         public void RenderFrame()
         {
             _renderer.SetDrawColor(0, 0, 0, 255);
             _renderer.ClearScreen();
-            
+ 
             _renderer.CameraLookAt(_player.Position.X, _player.Position.Y);
-
+ 
             RenderTerrain();
             RenderAllObjects();
-
+ 
             _renderer.PresentFrame();
         }
-
+ 
         private Tile? GetTile(int id)
         {
             if (_currentLevel == null) return null;
@@ -153,17 +156,17 @@ namespace TheAdventure
                     }
                 }
             }
-
+ 
             return null;
         }
-
+ 
         private void RenderTerrain()
         {
             if (_currentLevel == null) return;
             for (var layer = 0; layer < _currentLevel.Layers.Length; ++layer)
             {
                 var cLayer = _currentLevel.Layers[layer];
-
+ 
                 for (var i = 0; i < _currentLevel.Width; ++i)
                 {
                     for (var j = 0; j < _currentLevel.Height; ++j)
@@ -171,17 +174,17 @@ namespace TheAdventure
                         var cTileId = cLayer.Data[j * cLayer.Width + i] - 1;
                         var cTile = GetTile(cTileId);
                         if (cTile == null) continue;
-
+ 
                         var src = new Rectangle<int>(0, 0, cTile.ImageWidth, cTile.ImageHeight);
                         var dst = new Rectangle<int>(i * cTile.ImageWidth, j * cTile.ImageHeight, cTile.ImageWidth,
                             cTile.ImageHeight);
-
+ 
                         _renderer.RenderTexture(cTile.InternalTextureId, src, dst);
                     }
                 }
             }
         }
-
+ 
         private IEnumerable<RenderableGameObject> GetAllRenderableObjects()
         {
             foreach (var gameObject in _gameObjects.Values)
@@ -192,7 +195,7 @@ namespace TheAdventure
                 }
             }
         }
-
+ 
         private IEnumerable<TemporaryGameObject> GetAllTemporaryGameObjects()
         {
             foreach (var gameObject in _gameObjects.Values)
@@ -203,22 +206,39 @@ namespace TheAdventure
                 }
             }
         }
-
+ 
         private void RenderAllObjects()
         {
             foreach (var gameObject in GetAllRenderableObjects())
             {
                 gameObject.Render(_renderer);
             }
-
+ 
             _player.Render(_renderer);
         }
-
+ 
+        private void HiddenAbility(int x, int y, bool translateCoordinates = true)
+        {
+            var translated = translateCoordinates ? _renderer.TranslateFromScreenToWorldCoordinates(x, y) : new Vector2D<int>(x, y);
+ 
+            var newX = translated.X;
+            var newY = translated.Y;
+ 
+            var spriteSheet = SpriteSheet.LoadSpriteSheet("bomb.json", "Assets", _renderer);
+            if(spriteSheet != null){
+                spriteSheet.ActivateAnimation("Explode");
+                TemporaryGameObject bomb = new(spriteSheet, 2.1, (this._player.Position.X, this._player.Position.Y));
+                _gameObjects.Add(bomb.Id, bomb);
+            }
+ 
+            this._player.Position = (newX, newY);
+ 
+        }
         private void AddBomb(int x, int y, bool translateCoordinates = true)
         {
-
+ 
             var translated = translateCoordinates ? _renderer.TranslateFromScreenToWorldCoordinates(x, y) : new Vector2D<int>(x, y);
-            
+ 
             var spriteSheet = SpriteSheet.LoadSpriteSheet("bomb.json", "Assets", _renderer);
             if(spriteSheet != null){
                 spriteSheet.ActivateAnimation("Explode");
--- Input.cs
+++ Input.cs
@@ -1,5 +1,5 @@
 using Silk.NET.SDL;
-
+ 
 namespace TheAdventure
 {
     public unsafe class Input
@@ -7,52 +7,54 @@ namespace TheAdventure
         private Sdl _sdl;
         private GameWindow _gameWindow;
         private GameRenderer _renderer;
-        
+ 
         byte[] _mouseButtonStates = new byte[(int)MouseButton.Count];
-        
+ 
         public EventHandler<(int x, int y)> OnMouseClick;
-        
+ 
+        public EventHandler<(int x, int y)> OnRightMouseClick;
+ 
         public Input(Sdl sdl, GameWindow window, GameRenderer renderer)
         {
             _sdl = sdl;
             _gameWindow = window;
             _renderer = renderer;
         }
-
+ 
         public bool IsKeyAPressed(){
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.A] == 1;
         }
-
+ 
         public bool IsKeyBPressed(){
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.B] == 1;
         }
-
+ 
         public bool IsLeftPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Left] == 1;
         }
-        
+ 
         public bool IsRightPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Right] == 1;
         }
-        
+ 
         public bool IsUpPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Up] == 1;
         }
-        
+ 
         public bool IsDownPressed()
         {
             ReadOnlySpan<byte> _keyboardState = new(_sdl.GetKeyboardState(null), (int)KeyCode.Count);
             return _keyboardState[(int)KeyCode.Down] == 1;
         }
-        
+ 
         public bool ProcessInput()
         {
             var currentTime = DateTimeOffset.UtcNow;
@@ -65,7 +67,7 @@ namespace TheAdventure
                 {
                     return true;
                 }
-
+ 
                 switch (ev.Type)
                 {
                     case (uint)EventType.Windowevent:
@@ -119,24 +121,24 @@ namespace TheAdventure
                                 {
                                     _sdl.SetWindowInputFocus(_sdl.GetWindowFromID(ev.Window.WindowID));
                                 }
-
+ 
                                 break;
                             }
                         }
-
+ 
                         break;
                     }
-
+ 
                     case (uint)EventType.Fingermotion:
                     {
                         break;
                     }
-
+ 
                     case (uint)EventType.Mousemotion:
                     {
                         break;
                     }
-
+ 
                     case (uint)EventType.Fingerdown:
                     {
                         _mouseButtonStates[(byte)MouseButton.Primary] = 1;
@@ -147,44 +149,48 @@ namespace TheAdventure
                         mouseX = ev.Motion.X;
                         mouseY = ev.Motion.Y;
                         _mouseButtonStates[ev.Button.Button] = 1;
-                        
+ 
                         if (ev.Button.Button == (byte)MouseButton.Primary)
                         {
                             OnMouseClick?.Invoke(this, (mouseX, mouseY));
                         }
-                        
+ 
+                        else if (ev.Button.Button == (byte)MouseButton.Secondary)
+                        {
+                            OnRightMouseClick?.Invoke(this, (mouseX, mouseY));
+                        }
                         break;
                     }
-
+ 
                     case (uint)EventType.Fingerup:
                     {
                         _mouseButtonStates[(byte)MouseButton.Primary] = 0;
                         break;
                     }
-
+ 
                     case (uint)EventType.Mousebuttonup:
                     {
                         _mouseButtonStates[ev.Button.Button] = 0;
                         break;
                     }
-
+ 
                     case (uint)EventType.Mousewheel:
                     {
                         break;
                     }
-
+ 
                     case (uint)EventType.Keyup:
                     {
                         break;
                     }
-
+ 
                     case (uint)EventType.Keydown:
                     {
                         break;
                     }
                 }
             }
-
+ 
             return false;
         }
     }
